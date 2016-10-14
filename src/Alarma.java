6859999408e8f976f0c5d2e250f60b7521fd88ac
SergioMarchena <SergioMarchena@172.20.7.151>
2016-10-14 08:32:26 -0600

    Este es el ejercicio 1 (el pasado era el ejercicio 2)

--- src/Alarma.java
+++ src/Alarma.java
@@ -1,14 +1,8 @@
 
 public class Alarma extends Timbre {
 	public void m1() {
-		System.out.println("alarma 1");
+		System.out.println("Alarma 1");
 	}
 	 
-	public void m2() {
-		super.m1();
-	}
-	 
-	public String toString() {
-		return super.toString() + "â€¦" + super.toString();
-	}
+	
 }
