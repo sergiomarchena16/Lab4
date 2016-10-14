d256287a6dc7c9c3e14d6947515eb55ec37f3455
SergioMarchena <SergioMarchena@172.20.7.151>
2016-10-14 08:45:54 -0600

    Este es el ejercicio 3

--- src/Alarma.java
+++ src/Alarma.java
@@ -1,8 +1,13 @@
-
 public class Alarma extends Timbre {
 	public void m1() {
-		System.out.println("Alarma 1");
+		System.out.println("alarma 1");
 	}
 	 
-	
+	public void m2() {
+		super.m1();
+	}
+	 
+	public String toString() {
+		return super.toString() + "â€¦" + super.toString();
+	}
 }
---
+++ src/AlarmaEmergencia.java
@@ -0,0 +1,15 @@
+
+public class AlarmaEmergencia extends Alarma{
+	
+	public void m1(){
+		System.out.println("emergencia 1");
+	}
+	public void m2(){
+		super.m1();
+		super.m2();
+	}
+	public String toString(){
+		return "emergencia "+super.toString();
+	}
+
+}
